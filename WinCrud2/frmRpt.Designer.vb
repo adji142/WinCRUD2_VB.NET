652e842971b88d6992d24f6e76c383c889389418
adji142 <prasetyoajiw@gmail.com>
2019-03-01 16:31:32 +0700

    add some fitures

--- WinCrud2/frmRpt.Designer.vb
+++ WinCrud2/frmRpt.Designer.vb
@@ -22,16 +22,31 @@ Partial Class frmRpt
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
         Me.SuspendLayout()
+        '
+        'CrystalReportViewer1
+        '
+        Me.CrystalReportViewer1.ActiveViewIndex = -1
+        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CrystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(506, 270)
+        Me.CrystalReportViewer1.TabIndex = 0
+        Me.CrystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
         '
         'frmRpt
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(506, 270)
+        Me.Controls.Add(Me.CrystalReportViewer1)
         Me.Name = "frmRpt"
         Me.Text = "frmRpt"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
 End Class
--- WinCrud2/frmRpt.vb
+++ WinCrud2/frmRpt.vb
@@ -1,7 +1,30 @@
-﻿Public Class frmRpt
+﻿Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
 
+Imports WinCrud2.Test.Data
+Public Class frmRpt
+    Dim ds As DataSet
     Private Sub frmRpt_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Server As New ConnectionInfo
+        Dim DataTable As Table
 
-        Me.ReportViewer1.RefreshReport()
+
+
+        Me.Cursor = Cursors.WaitCursor
+
+        Dim DBX As Object = New DBConnection().ConnectionSetting()
+        With Server
+            .ServerName = "Localhost"
+            .DatabaseName = "PP"
+            .UserID = "Root"
+            .Password = "lagis3nt0s4"
+        End With
+
+        Dim ReportObject As New ReportDocument
+
+        ReportObject.Load("RptDoPrint.rpt")
+
+
+
     End Sub
 End Class
